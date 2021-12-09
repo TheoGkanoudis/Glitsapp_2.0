d65671f8f96616900b5a11b74b130133aa7f015f
Theo Gkanoudis <thodoris.gkanoudis@gmail.com>
2021-12-09 18:50:42 +0200

    added user location, if located within interest area, zooms in to user location (not quite tested yet)

--- app/src/main/java/com/example/glitsapp20/MapsActivity.java
+++ app/src/main/java/com/example/glitsapp20/MapsActivity.java
@@ -3,18 +3,38 @@ package com.example.glitsapp20;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+
+import android.location.LocationProvider;
+import android.os.Bundle;
+
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
 import android.graphics.Camera;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.JsonWriter;
 import android.util.Log;
 import android.util.LogPrinter;
+import android.webkit.PermissionRequest;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.internal.ICancelToken;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -25,6 +45,11 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.LocationSource;
+import com.google.android.gms.tasks.CancellationToken;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnTokenCanceledListener;
+import com.google.android.gms.tasks.Task;
 import com.google.maps.android.data.geojson.GeoJsonLayer;
 import com.google.maps.android.data.kml.KmlLayer;
 
@@ -38,22 +63,25 @@ import org.xmlpull.v1.XmlPullParserException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.security.Permission;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnCameraMoveListener, GoogleMap.OnPolylineClickListener, GoogleMap.OnMapClickListener {
+
+public class MapsActivity extends FragmentActivity
+        implements
+        OnMapReadyCallback,
+        GoogleMap.OnCameraMoveListener,
+        GoogleMap.OnPolylineClickListener,
+        GoogleMap.OnMapClickListener,
+        GoogleMap.OnMyLocationButtonClickListener,
+        GoogleMap.OnMapLongClickListener,
+        ActivityCompat.OnRequestPermissionsResultCallback,
+        GoogleMap.OnMyLocationClickListener {
 
     private GoogleMap mMap;
-    //TODO ||FUTURE|| MAYBE ADD MORE COLORS - maximum paths: number of colors below
-
-    private static final int[] PATH_COLORS = {0xffff1744,0xff00acc1,0xff9c27b0,0xffc51162,0xff5e35b1,0xff304ffe};
-    private static final double MIN_PATHS_VISIBLE_ZOOM = 11.2;
-    private static final double MIN_MARKERS_VISIBLE_ZOOM = 12.3;
-    private static final int PATH_UNSELECTED_W = 4;
-    private  static final int PATH_SELECTED_W = 7;
-
     ArrayList<String> pathNames = new ArrayList<>();
     ArrayList<String> POIPathNames = new ArrayList<>();
     List<LatLng[]> Prows = new ArrayList<LatLng[]>();
@@ -62,12 +90,47 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     LatLng[] Mline;
     ArrayList<Polyline> polylines = new ArrayList<>();
     ArrayList<Marker> markers = new ArrayList<>();
-    public boolean[] visibleMarkers;
-    public int pathSelected = -1;
 
+    public boolean permissionDenied = false;
+    public boolean[] visibleMarkers;
+    private boolean locationPermissionsGranted = false;
+    public int pathSelected = -1;
+    private Location lastKnownLocation;
+    private FusedLocationProviderClient fusedLocationClient;
+
+    LatLng apanoMeria = new LatLng(37.49913, 24.907264);
+    LatLng cameraPosition = apanoMeria;
+    Location userLocation;
+
+    // FINALS //
+
+    private static final int[] PATH_COLORS = {0xffff1744, 0xff00acc1, 0xff9c27b0, 0xffc51162, 0xff5e35b1, 0xff304ffe};
+    private static final double MIN_PATHS_VISIBLE_ZOOM = 11.2;
+    private static final double MIN_MARKERS_VISIBLE_ZOOM = 12.3;
+    private static final int PATH_UNSELECTED_W = 4;
+    private static final int PATH_SELECTED_W = 7;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    //keys for storing activity state
+    public static final String KEY_LOCATION = "location";
+    public static final String KEY_CAMERA_POSITION = "camera_position";
+
+
+    // DEFAULT //
+
+    //operational
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (savedInstanceState != null) {
+            lastKnownLocation = savedInstanceState.getParcelable(KEY_LOCATION);
+            cameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
+            userLocation = lastKnownLocation;
+            Toast.makeText(this, "idk", Toast.LENGTH_SHORT).show();
+        }
+
+
 
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -76,22 +139,65 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
         makeFeature("paths.json", Prows, Pline, pathNames);
         makeFeature("markers.json", Mrows, Mline, POIPathNames);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation();
+        } else {
+            // Permission was denied. Display an error message
+            // [START_EXCLUDE]
+            // Display the missing permission error dialog when the fragments resume.
+            permissionDenied = true;
+            // [END_EXCLUDE]
+        }
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (permissionDenied) {
+            //TODO Permission was not granted, display error dialog.
+            showMissingPermissionError();
+            permissionDenied = false;
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        if (mMap != null) {
+            outState.putParcelable(KEY_CAMERA_POSITION, mMap.getCameraPosition());
+            outState.putParcelable(KEY_LOCATION, lastKnownLocation);
+        }
+        super.onSaveInstanceState(outState);
+    }
+    //map
+    @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        LatLng apanoMeria = new LatLng(37.49913, 24.907264);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(apanoMeria));
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(13));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cameraPosition,13));
         mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
         mMap.setOnCameraMoveListener(this);
         mMap.setOnPolylineClickListener(this);
         mMap.setOnMapClickListener(this);
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
+
+        enableMyLocation();
         drawPaths();
         drawMarkers();
+
+        getDeviceLocation();
     }
 
     @Override
@@ -99,7 +205,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     {
         zoomRethink();
     }
-
 
     @Override
     public void onPolylineClick(@NonNull Polyline polyline) {
@@ -111,8 +216,65 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if(pathSelected>=0){pathRethink(polylines.get(pathSelected));}
     }
 
-    //TODO incorporate user location
-    //TODO make paths clickable - center on path when touched
+    @Override
+    public boolean onMyLocationButtonClick() {
+        return false;
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+
+    }
+
+    @Override
+    public void onMapLongClick(@NonNull LatLng latLng) {
+
+    }
+
+
+    // LOCATION //
+
+    private void enableMyLocation(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            if (mMap != null) {
+                mMap.setMyLocationEnabled(true);
+                }
+            locationPermissionsGranted = true;
+        } else {
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        }
+    }
+
+    private void getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (locationPermissionsGranted) {
+                Task<Location> locationResult = fusedLocationClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful()) {
+                            lastKnownLocation = task.getResult();
+                            if(lastKnownLocation.getLatitude()<37.514961&&lastKnownLocation.getLatitude()>37.479590&&
+                                    lastKnownLocation.getLongitude()<24.946950&&lastKnownLocation.getLongitude()>24.875893){
+                                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lastKnownLocation.getLatitude(),lastKnownLocation.getLongitude()),(float) 14.7));
+                            }
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage(), e);
+        }
+    }
+
+
+    // FEATURES //
 
     private void makeFeature(String filename, List<LatLng[]> rows, LatLng[] line, ArrayList<String> names){
         try {
@@ -174,6 +336,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
         visibleMarkers = new boolean[markers.size()];
     }
+
+    // ADJUSTMENTS //
 
     private void zoomRethink(){
         //paths
@@ -248,6 +412,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng), (float) 13.65));
     }
 
+    // OPERATIONAL //
+
     private String jsonFromAssets(String fileName) {
         String json = null;
         try {
@@ -264,5 +430,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
         return json;
     }
+
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(getSupportFragmentManager(), "dialog");
+    }
+
 
 }
